55f6e8c406238e87140d24baf8d19c205adde29c
nvenkats <nvenki505@gmail.com>
2017-06-01 19:26:02 +0530

    Ability to store and display notes is working.
    Have to write methods for deleting and editing

--- functionality.js
+++ functionality.js
@@ -1,3 +1,4 @@
+var items = [];
 window.onload = function() {
     document.getElementById('menulink').onclick = function() {
         var menu = document.getElementById('menu');
@@ -21,11 +22,14 @@ window.onload = function() {
 		var text = document.createTextNode("This app lets you create, edit and delete notes. All notes are stored in your local storage and hence they will be available even if you restart your browser. Happy organizing!");
 		p.appendChild(text);
 	}
+	
 	var home = document.getElementById('container').innerHTML;
 	var controls1 = document.getElementById('controls1').innerHTML;
 	var output = document.getElementById('output').innerHTML;
 	var overall_controls = document.getElementById('overall_controls').innerHTML;
+
 	display_saved_note();
+
 	document.getElementById('home').onclick = function() {
 		document.getElementById('container').innerHTML = home;
 		document.getElementById('controls1').innerHTML = controls1;
@@ -45,58 +49,73 @@ function check_web_storage_support() {
     }
 }
 function delete_all() {
-	
+	var i;
+    for(i = document.getElementById("outputNote").options.length - 1 ; i >= 0 ; i--)
+    {
+        document.getElementById("outputNote").remove(i);
+    }
 }
-function sort() {
+function sortNote() {
 	
 }
 function delete_note() {
-	
+  
 }
-function edit() {
-	
+function editNote() {
+	 
 }
-var options = [];
 function display_saved_note() {
-    if(check_web_storage_support() == true) {
-        items = JSON.parse(localStorage.getItem('item'));
-    }
-    if(items === null) {
-		var y = document.getElementById("outputNote");
-		var option = document.createElement("option");
-		option.text = "No saved notes.";
-		y.add(option); 
-	}
+	
 	var i;
-	for(i=0; i<items.length; i++) {
-		options[i] = items[i].substr(1);
-	}
-	for(i=0; i<options.length; i++) {
-		y = document.getElementById("outputNote");
-		option = document.createElement("option");
-		option.text = options[i];
-		y.add(option);
-	}
+    for(i = document.getElementById("outputNote").options.length - 1 ; i >= 0 ; i--)
+    {
+        document.getElementById("outputNote").remove(i);
+    }	
+
+	if(check_web_storage_support() === true) {
+		if(JSON.parse(localStorage.getItem("item"))!== null)  
+			items = JSON.parse(localStorage.getItem("item"));
+		
+  
+		for(j=0; j<items.length; j++) {
+			var y1 = document.getElementById("outputNote");
+			var option1 = document.createElement("option");
+			option1.text = items[j].substr(1); 
+			y1.add(option1);  
+		}
+    }
+	
 }
-var items = [];
-var i = 0;
-function save() {
-    if(check_web_storage_support() == true) {
-        var area = document.getElementById("area");
-		var p = document.getElementById("priority").length.toString();
-		var item = p.concat(area.value);
-        if(area.value != '') {
-			items.push(item); 
-			i++;
-			localStorage.setItem("item", JSON.stringify(items));
-			var x = document.getElementById("outputNote");
-			var option = document.createElement("option");
-			option.text = area.value;
-			x.add(option);
-        }
-        else {
-            alert("Nothing to save");
-        }
+
+function saveNote() {
+    if(check_web_storage_support() === true) {
+		
+		var items = [];
+		var area = document.getElementById("area");
+		var pr = document.getElementById("priority").value.length.toString();
+		var nt = pr.concat(area.value);
+		if(JSON.parse(localStorage.getItem("item")) !== null) {
+			items = JSON.parse(localStorage.getItem("item"));
+
+		  
+			
+			if(area.value !== '') { 
+							
+				items.push(nt);
+				localStorage.setItem("item", JSON.stringify(items));
+				display_saved_note();
+			}
+			
+			else {
+				alert("Nothing to save");
+			}
+		  }
+		  else 
+			{	
+			  
+				items.push(nt);
+				localStorage.setItem("item", JSON.stringify(items));
+				display_saved_note();         }
     }
 }
 function clean() {
